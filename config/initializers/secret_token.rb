b6904a40354655c448edfc8d934f1923890aa7ca
cs169-group28 <cm91228@yahoo.com>
2012-10-14 16:46:42 -0700

    please work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerfSONAR::Application.config.secret_token = '4787c4bfdcc7ce1b19b9d78fc0284ac1a36148f7f20fe6a2734020a4cf2a76d1d0af5404bfe0cc983bdac610f2f5eeb8ffcac100044802e4c19628500adbc2d0'
