3bfbe3a96ccef0d0e64d0f878435debd86dd0570
Gautam Jain <gautamjain@gmail.com>
2012-10-21 22:26:23 -0700

    Updated commit - traceroute functionality

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,10 +2,14 @@ class Server < ActiveRecord::Base
   attr_accessible :city, :country, :ip, :latitude, :longitude, :name, :state, :zip
 
   before_create :getIp
+  acts_as_gmappable :process_geocoding => false
 
   require 'open-uri'
 
 
+  def gmaps4rails_address
+    {latitude:self.latitude, longitude:self.longitude}
+  end
 
   def getIpInfo(ip)
   @key = '61acd0537857ef094869ff7dabe7571e6f800cb2a8a5e9dd5a5d8340f402982a'
--- app/models/traceroute.rb
+++ app/models/traceroute.rb
@@ -1,3 +1,12 @@
-class Traceroute < ActiveRecord::Base
+class Traceroute 
   # attr_accessible :title, :body
+
+  # acts_as_gmappable :process_geocoding => false
+
+
+
+  # def gmaps4rails_address
+  #   {latitude:self.latitude, longitude:self.longitude}
+  # end
+
 end
