0897ac8d3678eb1cbbe9d7b79ccf81ff0aa8eecc
Gautam Jain <gautamjain@gmail.com>
2012-11-19 00:27:25 -0800

    Removed unnecessary print statements.

--- app/models/perfsonar.rb
+++ app/models/perfsonar.rb
@@ -102,11 +102,6 @@ class Perfsonar
 	private
 
 	def self.postToPerfSonar(domain, data)
-		p '========'
-		p data.to_xml
-		p '========'
-		p domain
-		#domain = 'lbl-pt1.es.net'
 		url = URI.parse(domain)
 		request = Net::HTTP::Post.new(url.path)
 		request.body = data.to_xml
